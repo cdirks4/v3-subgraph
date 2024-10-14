7b176a3ff7e1771fc60ce1ade130e2c03d713209
Connor Dirks <cdirks4@me.com>
2024-10-14 13:58:45 -0400

    feat: update for story-testnet

--- src/utils/chains.ts
+++ src/utils/chains.ts
@@ -14,6 +14,7 @@ export enum ChainId {
   MATIC = 137,
   OPTIMISM = 10,
   ZKSYNC_ERA = 324,
+  STORY_TESTNET = 1513,
 }
 
 // subgraph does not support string enums, hence these constants
@@ -27,7 +28,7 @@ const MAINNET_NETWORK_NAME = 'mainnet'
 const MATIC_NETWORK_NAME = 'matic'
 const OPTIMISM_NETWORK_NAME = 'optimism'
 const ZKSYNC_ERA_NETWORK_NAME = 'zksync-era'
-
+const STORY_TESTNET_NAME = 'story-testnet'
 // Note: All token and pool addresses should be lowercased!
 export class SubgraphConfig {
   // deployment address
@@ -110,6 +111,28 @@ export function getSubgraphConfig(): SubgraphConfig {
       poolsToSkip: [],
       poolMappings: [],
     }
+  } else if (selectedNetwork == STORY_TESTNET_NAME) {
+    return {
+      factoryAddress: '0x2D1bc93A4d0C23047577ac16C7Fe5CfEAb62B87B',
+      stablecoinWrappedNativePoolAddress: '0x8d6934472ea18591f4de85bd04110e2d152e2a11', // WETH-USDbC 0.05% pool
+      stablecoinIsToken0: false,
+      wrappedNativeAddress: '0x968B9a5603ddEb2A78Aa08182BC44Ece1D9E5bf0', // WETH
+      minimumNativeLocked: BigDecimal.fromString('1'),
+      stablecoinAddresses: [
+        '0x700722D24f9256Be288f56449E8AB1D27C4a70ca', // USDC
+        '0x8812d810EA7CC4e1c3FB45cef19D6a7ECBf2D85D', // USDT
+      ],
+      whitelistTokens: [
+        '0x700722D24f9256Be288f56449E8AB1D27C4a70ca', // USDC
+        '0x48082875C79f1E92bB3A685C28eCC3035c3BD8a8', // WIP
+        '0x968B9a5603ddEb2A78Aa08182BC44Ece1D9E5bf0', // WETH
+        '0x153B112138C6dE2CAD16D66B4B6448B7b88CAEF3', // WBTC
+        '0xE42849EE7bd8F02B29deD238f6E023aBFA67d438', // FATE
+      ],
+      tokenOverrides: [],
+      poolsToSkip: [],
+      poolMappings: [],
+    }
   } else if (selectedNetwork == AVALANCHE_NETWORK_NAME) {
     return {
       factoryAddress: '0x740b1c1de25031C31FF4fC9A62f554A55cdC1baD',
@@ -134,42 +157,6 @@ export function getSubgraphConfig(): SubgraphConfig {
         '0xc7198437980c041c805a1edcba50c1ce5db95118', // USDT_E
         '0x9702230a8ea53601f5cd2dc00fdbc13d4df4a8c7', // USDT
         '0x130966628846bfd36ff31a822705796e8cb8c18d', // MIM
-      ],
-      tokenOverrides: [],
-      poolsToSkip: [],
-      poolMappings: [],
-    }
-  } else if (selectedNetwork == BASE_NETWORK_NAME) {
-    return {
-      factoryAddress: '0x33128a8fC17869897dcE68Ed026d694621f6FDfD',
-      stablecoinWrappedNativePoolAddress: '0x4c36388be6f416a29c8d8eee81c771ce6be14b18', // WETH-USDbC 0.05% pool
-      stablecoinIsToken0: false,
-      wrappedNativeAddress: '0x4200000000000000000000000000000000000006', // WETH
-      minimumNativeLocked: BigDecimal.fromString('1'),
-      stablecoinAddresses: [
-        '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', // USDC
-      ],
-      whitelistTokens: [
-        '0x4200000000000000000000000000000000000006', // WETH
-        '0x833589fcd6edb6e08f4c7c32d4f71b54bda02913', // USDC
-      ],
-      tokenOverrides: [],
-      poolsToSkip: [],
-      poolMappings: [],
-    }
-  } else if (selectedNetwork == BLAST_MAINNET_NETWORK_NAME) {
-    return {
-      factoryAddress: '0x792edAdE80af5fC680d96a2eD80A44247D2Cf6Fd',
-      stablecoinWrappedNativePoolAddress: '0xf52b4b69123cbcf07798ae8265642793b2e8990c', // USDB-WETH 0.3% pool
-      stablecoinIsToken0: true,
-      wrappedNativeAddress: '0x4300000000000000000000000000000000000004', // WETH
-      minimumNativeLocked: BigDecimal.fromString('1'),
-      stablecoinAddresses: [
-        '0x4300000000000000000000000000000000000003', // USDB
-      ],
-      whitelistTokens: [
-        '0x4300000000000000000000000000000000000004', // WETH
-        '0x4300000000000000000000000000000000000003', // USDB
       ],
       tokenOverrides: [],
       poolsToSkip: [],
@@ -238,27 +225,8 @@ export function getSubgraphConfig(): SubgraphConfig {
         '0x956f47f50a910163d8bf957cf5846d573e7f87ca', // FEI
       ],
       whitelistTokens: [
-        '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
-        '0x6b175474e89094c44da98b954eedeac495271d0f', // DAI
-        '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48', // USDC
-        '0xdac17f958d2ee523a2206206994597c13d831ec7', // USDT
-        '0x0000000000085d4780b73119b644ae5ecd22b376', // TUSD
-        '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599', // WBTC
-        '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-        '0x39aa39c021dfbae8fac545936693ac917d5e7563', // cUSDC
-        '0x86fadb80d8d2cff3c3680819e4da99c10232ba0f', // EBASE
-        '0x57ab1ec28d129707052df4df418d58a2d46d5f51', // sUSD
-        '0x9f8f72aa9304c8b593d555f12ef6589cc3a579a2', // MKR
-        '0xc00e94cb662c3520282e6f5717214004a7f26888', // COMP
-        '0x514910771af9ca656af840dff83e8264ecf986ca', // LINK
-        '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f', // SNX
-        '0x0bc529c00c6401aef6d220be8c6ea1667f6ad93e', // YFI
-        '0x111111111117dc0aa78b770fa6a738034120c302', // 1INCH
-        '0xdf5e0e81dff6faf3a7e52ba697820c5e32d806a8', // yCurv
-        '0x956f47f50a910163d8bf957cf5846d573e7f87ca', // FEI
-        '0x7d1afa7b718fb893db30a3abc0cfc608aacfebb0', // MATIC
-        '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9', // AAVE
-        '0xfe2e637202056d30016725477c5da089ab0a043a', // sETH2
+        '0x700722D24f9256Be288f56449E8AB1D27C4a70ca', // USDC
+        '0x153B112138C6dE2CAD16D66B4B6448B7b88CAEF3', // WBTC
       ],
       tokenOverrides: [
         {
@@ -303,7 +271,7 @@ export function getSubgraphConfig(): SubgraphConfig {
     }
   } else if (selectedNetwork == MATIC_NETWORK_NAME) {
     return {
-      factoryAddress: '0x1F98431c8aD98523631AE4a59f267346ea31F984',
+      factoryAddress: '0x82FFccAa0d16C8FB0C5A433Ffec709A2fbCa3cB9',
       stablecoinWrappedNativePoolAddress: '0xa374094527e1673a86de625aa59517c5de346d32', // WMATIC-USDC 0.05% pool
       stablecoinIsToken0: false,
       wrappedNativeAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270', // WMATIC
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,6 @@
-import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
+
+import { Factory as FactoryContract } from '../types/Factory/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
 
@@ -7,3 +9,5 @@ export const ONE_BI = BigInt.fromI32(1)
 export const ZERO_BD = BigDecimal.fromString('0')
 export const ONE_BD = BigDecimal.fromString('1')
 export const BI_18 = BigInt.fromI32(18)
+
+export const factoryContract = FactoryContract.bind(Address.fromString('0x2d1bc93a4d0c23047577ac16c7fe5cfeab62b87b'))
